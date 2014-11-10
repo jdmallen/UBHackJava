c42f64a1b0f8c6ac78d93324d81cf5dfda350b5c
lightheat <lightandtheheat@gmail.com>
2014-11-09 22:23:34 -0500

    adding frontend frameworks

---
+++ src/com/jdmallen/ambutag/actions/IndexAction.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.jdmallen.ambutag.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * @author Jesse
+ *
+ */
+public class IndexAction extends ActionSupport {
+
+}
